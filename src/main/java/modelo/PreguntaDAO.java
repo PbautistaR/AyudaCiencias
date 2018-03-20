c0377abccee097b94265506c026af611269ad590
Alan0500 <marco_montano@ciencias.unam.mx>
2018-03-20 15:49:25 -0600

    DAOS

---
+++ src/main/java/modelo/PreguntaDAO.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Alan
+ */
+public class PreguntaDAO {
+    private SessionFactory sessionFactory;
+    /**
+     * Constructor de la clase.
+     * inicializa la variable sessionFactory para que podamos abrir una session
+    */
+    public PreguntaDAO(){
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+    
+    public boolean nuevaPregunta(Pregunta u){
+         // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            session.persist(u);
+            
+            tx.commit();
+        }
+        catch (Exception er) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           er.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return true;
+    
+    }
+    
+    public boolean eliminaPregunta(Pregunta u){
+         // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            session.delete(u);
+            
+            tx.commit();
+        }
+        catch (Exception er) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           er.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return true;
+    
+    }
+    
+    public boolean actualizaPregunta(Pregunta u){
+         // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            session.update(u);
+            
+            tx.commit();
+        }
+        catch (Exception er) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           er.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return true;
+    
+    }
+}
---
+++ src/main/java/modelo/UsuarioDAO.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Alan
+ */
+public class UsuarioDAO {
+    
+    private SessionFactory sessionFactory;
+    /**
+     * Constructor de la clase.
+     * inicializa la variable sessionFactory para que podamos abrir una session
+    */
+    public UsuarioDAO(){
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+    
+    public boolean nuevoUsuario(Usuario u){
+         // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            session.persist(u);
+            
+            tx.commit();
+        }
+        catch (Exception er) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           er.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return true;
+    
+    }
+    
+    public boolean eliminaUsuario(Usuario u){
+         // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            session.delete(u);
+            
+            tx.commit();
+        }
+        catch (Exception er) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           er.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return true;
+    
+    }
+    
+    public boolean actualizaUsuario(Usuario u){
+         // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            session.update(u);
+            
+            tx.commit();
+        }
+        catch (Exception er) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           er.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return true;
+    
+    }
+}
